9c74dc4782a6d728597c42bd1d246c4f13e3da57
santhoshkumar.s <santhoshkumar.s@zohocorp.com>
2017-06-13 22:34:40 +0530

    material design applied

---
+++ XYZReader/src/main/java/com/example/xyzreader/ui/FABBehaviour.java
@@ -0,0 +1,66 @@
+package com.example.xyzreader.ui;
+
+import android.content.Context;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.view.ViewCompat;
+import android.support.v4.view.ViewPropertyAnimatorListener;
+import android.support.v4.view.animation.FastOutSlowInInterpolator;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+
+/**
+ * Created by santhosh-3366 on 13/06/17.
+ */
+
+public class FABBehaviour extends FloatingActionButton.Behavior {
+
+    public FABBehaviour(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onStartNestedScroll(CoordinatorLayout coordinatorLayout, FloatingActionButton child, View directTargetChild, View target, int nestedScrollAxes) {
+        Log.e("fired","start");
+        return nestedScrollAxes == ViewCompat.SCROLL_AXIS_VERTICAL || super.onStartNestedScroll(coordinatorLayout, child, directTargetChild, target, nestedScrollAxes);
+    }
+
+
+
+    @Override
+    public void onNestedScroll(CoordinatorLayout coordinatorLayout, FloatingActionButton child, View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed) {
+        Log.e("fired","nested");
+        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed);
+
+        if (dyConsumed > 0 && child.getVisibility() == View.VISIBLE) {
+            animateOut(child);
+        } else if (dyConsumed < 0 && child.getVisibility() != View.VISIBLE) {
+            animateIn(child);
+        }
+    }
+
+    private void animateOut(final FloatingActionButton button) {
+        ViewCompat.animate(button).scaleX(0.0F).scaleY(0.0F).alpha(0.0F).setInterpolator(new FastOutSlowInInterpolator()).withLayer()
+                .setListener(new ViewPropertyAnimatorListener() {
+                    public void onAnimationStart(View view) {
+                    }
+
+                    public void onAnimationCancel(View view) {
+                    }
+
+                    public void onAnimationEnd(View view) {
+                        view.setVisibility(View.INVISIBLE);
+                    }
+                }).start();
+    }
+
+    // Same animation that FloatingActionButton.Behavior uses to show the FAB when the AppBarLayout enters
+    private void animateIn(FloatingActionButton button) {
+        button.setVisibility(View.VISIBLE);
+        ViewCompat.animate(button).scaleX(1.0F).scaleY(1.0F).alpha(1.0F)
+                .setInterpolator(new FastOutSlowInInterpolator()).withLayer().setListener(null)
+                .start();
+
+    }
+}
